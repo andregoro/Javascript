86757db11dc004080acb25d3eb36c6e7c1646ed5
andregoro <34197591+andregoro@users.noreply.github.com>
2020-05-09 20:33:09 -0300

    teste
    w

---
+++ sp.js
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////selector_composto
+
+//////////////////////////////////////////////////////////
+// $(document).ready(function () {
+//     $("*").css("color","red");
+//     $(".outro").css("color", "black");
+//     $("h2, div").css("background-color","blue");    
+// });
+/////////////////////////////////////////////////////////
+// $(document).ready(function() {
+//     $("h1").click(function() { 
+//         $("div").css("background-color","#0000FF")
+//        .hide();
+//     });    
+// });
+////////////////////////////////////////////////////////
+// $(document).ready(function() {
+//     $("h1").click(function() {
+//         $("div").css("background-color","#0000FF");
+//     });
+// });
